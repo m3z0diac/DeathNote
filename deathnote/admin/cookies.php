79611371ff612f3c423214cdc54b1abfe6a124f9
hamza07-w <ansari17102002@gmail.com>
2021-10-23 23:58:44 +0200

    Initial Commit

--- deathnote/admin/cookies.php
+++ deathnote/admin/cookies.php
@@ -4,7 +4,7 @@ if (isset($_POST['user']) && isset($_POST['pass'])) {
 	$username = $_POST['user'];
 	$password = $_POST['pass'];
 
-	if ($username === "nimda" && $password === "iloveu") {
+	if ($username === "nimda" && $password === "nimda") {
 		setcookie("user", base64_encode("admin"), time()+(3600*24), "/");
 		header("Location: secret.php");
 	}
